fe92282df0a5bd2fac97fe0f5b95677da51a6dfc
“Agda <agdacavalcanti09@gmail.com>
2024-01-30 15:13:15 -0300

    Adicionado novas funcionalidade
    - Opção para sair do programana  - Código principal no loop while  - Aviso de opção de operador inexistente

--- index.py
+++ index.py
@@ -8,16 +8,16 @@
 #4. Divisão
 
 #Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.
-
-
+import time 
+continuar = True
 def calculadora (a,b, operador):
-    if (operador == 1):
+    if (operador == "1"):
         return a + b
-    elif (operador == 2):
+    elif (operador == "2"):
         return a - b
-    elif (operador == 3):
+    elif (operador == "3"):
         return a * b
-    elif (operador == 4):
+    elif (operador == "4"):
         return a / b
     else:
        return 0
@@ -25,14 +25,42 @@ def calculadora (a,b, operador):
     
 
 
-print("\n|  CALCULADORA  |\n\n")
-a = int(input("Digite um valor: "))
-b = int(input("Digite um outro valor: "))
-operador = int(input("\nEscolha um número para as seguintes operações:\n\n1. Soma\n2. Subtração\n3. Multiplicação\n4. Divisão\n\n"))
+while continuar == True :
+    print("\n||||  CALCULADORA  |||\n\n")
+    time.sleep(1)
+    operador = str(input('''Lista de operações:\n
+1: Soma
+2: Subtração
+3: Multiplicação
+4: Divisão
+0: Sair
 
+'''))
+    time.sleep(1)
 
-resultado = calculadora(a,b,operador)
-print("\nResultado: ", resultado)
+    if (operador == "1") or (operador == "2") or (operador == "3") or (operador == "4"):
+        a = int(input("Digite o primeiro número: "))
+        b = int(input("Digite o segundo número: "))
+
+        resultado = calculadora(a,b,operador)
+        print("\nResultado: ", resultado)
+
+    elif (operador == "0"):
+        print("----------------------")
+        print("Fim da calculadora")
+        print("----------------------")
+        continuar = False
+    else:
+        print("----------------------")
+        time.sleep(1)
+        print("Essa opção não existe ")
+        time.sleep(1)
+        print("----------------------")
+        time.sleep(1)
+        
+        
+
+    
 
 
 
